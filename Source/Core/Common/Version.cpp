6f0c63e50d4cf267c865f929ae24c70e3e2cb948
BlueSwordM <zakdjeb@gmail.com>
2022-11-02 21:39:41 -0400

    Rebasing Dolphin ExGL another time

--- Source/Core/Common/Version.cpp
+++ Source/Core/Common/Version.cpp
@@ -19,15 +19,12 @@ namespace Common
 
 const std::string& GetScmRevStr()
 {
-  static const std::string scm_rev_str = "Dolphin "
-#if !SCM_IS_MASTER
-                                         "[" SCM_BRANCH_STR "] "
-#endif
+  static const std::string scm_rev_str = "ExGL-Dolphin V1.06 "
 
 #ifdef __INTEL_COMPILER
       BUILD_TYPE_STR SCM_DESC_STR "-ICC";
 #else
-      BUILD_TYPE_STR SCM_DESC_STR;
+      BUILD_TYPE_STR;
 #endif
   return scm_rev_str;
 }
